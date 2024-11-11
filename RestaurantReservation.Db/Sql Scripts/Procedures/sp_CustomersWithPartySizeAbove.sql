f1bb3ff0017db494236e03512433d830222c38e2
Abdalkreem Bzoor <abdalkreembzoor@gmail.com>
2024-11-11 18:16:18 +0200

    Added Sql Scripts folder

---
+++ RestaurantReservation.Db/Sql Scripts/Procedures/sp_CustomersWithPartySizeAbove.sql
@@ -0,0 +1,9 @@
+﻿CREATE OR ALTER PROCEDURE sp_CustomersWithPartySizeAbove (
+@Size INT
+)
+AS
+BEGIN
+     SELECT C.CustomerId, CONCAT([First Name], ' ', [Last Name]) AS [Name], Email, [Phone Number]
+	 FROM Customers C JOIN Reservations R ON C.CustomerId = R.CustomerId
+	 WHERE [Party Size] >= @Size;
+END;
---
+++ RestaurantReservation.Db/Sql Scripts/Views/EmployeeRestaurantDetails.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW EmployeeRestaurantDetails AS 
+SELECT E.EmployeeId, CONCAT([First Name], ' ', [Last Name]) AS [Name], E.Position,
+R.RestaurantId, R.[Name] AS [Restaurant Name], R.[Address], R.[Phone Number]
+FROM Restaurants R JOIN Employees E ON R.RestaurantId = E.RestaurantId;

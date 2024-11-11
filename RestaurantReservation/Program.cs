a770b27b23626c8e3477de7fae9676ddeb4d1366
Abdalkreem Bzoor <abdalkreembzoor@gmail.com>
2024-11-11 18:47:10 +0200

    demonstrate the functionality of all the methods by calling them with sample data for testing purposes, in RestaurantReservation Project in Program Main Method

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,14 +1,81 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Data;
 using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Repositories;
 
 namespace RestaurantReservation
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            
+            using (var context = new RestaurantReservationDbContext())
+            {
+                //var result = await new EmployeeRepository(context).ListManagersAsync();
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                //var result = await new ReservationRepository(context).GetReservationsByCustomerAsync(1);
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                //var result = await new OrderRepository(context).ListOrdersAndMenuItemsAsync(1);
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item.Key);
+                //    foreach (var r in item.Value)
+                //    {
+                //        Console.WriteLine(r?.ToString()?.PadLeft(50, ' '));
+                //    }
+                //}
+
+                //var result = await new MenuItemRepository(context).ListOrderedMenuItemsAsync(1);
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                //Console.WriteLine(await new OrderRepository(context).CalculateAverageOrderAmountAsync(2));
+
+                //var result = await new CustomerRepository(context).GetCustomersWithLargePartySizeAsync(6);
+
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                //var result = context.ReservationsCustomerRestaurantDetails.ToList();
+
+                //foreach (var item in result) Console.WriteLine(item);
+
+                //var result = context.EmployeeRestaurantDetails.ToList();
+
+                //foreach (var item in result) Console.WriteLine(item + "\n\n\n");
+
+
+                //var result = context.Restaurants.Select(r => new
+                //{
+                //    r.Name,
+                //    Revenue = RestaurantReservationDbContext.CalculateTotalRevenueByRestaurant(r.RestaurantId)
+                //}).ToList();
+
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine($"Restaurant Name : {item.Name}  |  Revenue = {item.Revenue}");
+                //}
+
+                //var result = await new CustomerRepository(context).GetCustomersWithLargePartySizeAsync(1);
+
+                //foreach (var item in result) Console.WriteLine(item);
+
+
+            }
+
+            Console.ReadKey();
         }
     }
 }

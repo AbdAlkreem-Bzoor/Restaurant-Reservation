41852928addf7dcd707434db133012d0923854c8
Abdalkreem Bzoor <abdalkreembzoor@gmail.com>
2024-11-11 18:00:38 +0200

    Added Migration

---
+++ RestaurantReservation.Db/Migrations/20241111155719_Added-Views-Functions-StoredProcedures.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedViewsFunctionsStoredProcedures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomersWithPartySizeAbove",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(name: "Phone Number", type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomersWithPartySizeAbove");
+        }
+    }
+}
